dc69814d30ebe74a99259fc7c25e894473bb13b8
phpcrazy <phpcrazy@gmail.com>
2021-10-05 15:55:27 +0630

    phpinfo()

--- src/public/index.php
+++ src/public/index.php
@@ -1,16 +1,3 @@
 <?php
 
-try {
-    $conn = new PDO(
-        "mysql:host=mysql;dbname=phpguy", 
-        'root',
-        'root',
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-    );
-    $statement = $conn->prepare("SELECT * FROM students");
-    $statement->execute();
-    $students = $statement->fetchAll(PDO::FETCH_OBJ);
-    var_dump($students);
-} catch (PDOException $e) {
-    echo $e->getMessage(), PHP_EOL;
-}
+phpinfo();
